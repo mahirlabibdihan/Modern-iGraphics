0304646aff29a9bf9fa2e96d92774315e027ccfc
shuaibw <1805010@ugrad.cse.buet.ac.bd>
2024-12-08 16:47:03 +0600

    sound demo

---
+++ soundDemo.cpp
@@ -0,0 +1,79 @@
+#include "iGraphics.h"
+
+
+void iDraw()
+{
+	//place your drawing codes here	
+	iClear();
+    iText(10, 10, "Left click to hear a chime", GLUT_BITMAP_HELVETICA_12);
+}
+
+/* 
+	function iMouseMove() is called when the user presses and drags the mouse.
+	(mx, my) is the position where the mouse pointer is.
+*/
+void iMouseMove(int mx, int my)
+{
+	//place your codes here
+}
+
+/*
+	function iPassiveMouseMove() is called automatically when the mouse pointer is in motion
+*/
+void iPassiveMouseMove(int mx, int my)
+{
+	//place your code here
+    // printf("x = %d, y = %d\n", mx, my);
+}
+
+/* 
+	function iMouse() is called when the user presses/releases the mouse.
+	(mx, my) is the position where the mouse pointer is.
+*/
+void iMouse(int button, int state, int mx, int my)
+{
+	if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		PlaySound("chime.wav", NULL, SND_FILENAME | SND_ASYNC);
+	}
+	if(button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
+	{
+	}
+}
+
+
+/*
+	function iKeyboard() is called whenever the user hits a key in keyboard.
+	key- holds the ASCII value of the key pressed. 
+*/
+void iKeyboard(unsigned char key)
+{
+
+	//place your codes for other keys here
+}
+
+/*
+	function iSpecialKeyboard() is called whenver user hits special keys like-
+	function keys, home, end, pg up, pg down, arraows etc. you have to use 
+	appropriate constants to detect them. A list is:
+	GLUT_KEY_F1, GLUT_KEY_F2, GLUT_KEY_F3, GLUT_KEY_F4, GLUT_KEY_F5, GLUT_KEY_F6, 
+	GLUT_KEY_F7, GLUT_KEY_F8, GLUT_KEY_F9, GLUT_KEY_F10, GLUT_KEY_F11, GLUT_KEY_F12, 
+	GLUT_KEY_LEFT, GLUT_KEY_UP, GLUT_KEY_RIGHT, GLUT_KEY_DOWN, GLUT_KEY_PAGE UP, 
+	GLUT_KEY_PAGE DOWN, GLUT_KEY_HOME, GLUT_KEY_END, GLUT_KEY_INSERT 
+*/
+void iSpecialKeyboard(unsigned char key)
+{
+
+	if(key == GLUT_KEY_END)
+	{
+		exit(0);	
+	}
+
+	//place your codes for other keys here
+}
+
+int main()
+{
+	iInitialize(600, 400, "Mousedemo");
+	return 0;
+}	
