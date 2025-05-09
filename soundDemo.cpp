1e76b57b3a0abce2b52450f7a3c361ea1386425e
mahirlabibdihan <mahirlabibdihan@gmail.com>
2025-05-09 22:11:49 +0600

    demo fix

--- soundDemo.cpp
+++ soundDemo.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 void iDraw()
 {
-	//place your drawing codes here	
+	// place your drawing codes here
 	iClear();
-    iText(10, 10, "Left click to hear a chime", GLUT_BITMAP_HELVETICA_12);
+	iText(10, 10, "Left click to hear a chime", GLUT_BITMAP_HELVETICA_12);
 }
 
-/* 
+/*
 	function iMouseDrag() is called when the user presses and drags the mouse.
 	(mx, my) is the position where the mouse pointer is.
 */
 void iMouseDrag(int mx, int my)
 {
-	//place your codes here
+	// place your codes here
 }
 
 /*
@@ -23,30 +23,29 @@ void iMouseDrag(int mx, int my)
 */
 void iMouseMove(int mx, int my)
 {
-	//place your code here
-    // printf("x = %d, y = %d\n", mx, my);
+	// place your code here
+	//  printf("x = %d, y = %d\n", mx, my);
 }
 
-/* 
+/*
 	function iMouse() is called when the user presses/releases the mouse.
 	(mx, my) is the position where the mouse pointer is.
 */
 void iMouse(int button, int state, int mx, int my)
 {
-	if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
 	{
-		iPlaySound("chime.wav",false);
-			// PlaySound("chime.wav", NULL, SND_FILENAME | SND_ASYNC);
+		iPlaySound("chime.wav", false);
+		// PlaySound("chime.wav", NULL, SND_FILENAME | SND_ASYNC);
 	}
-	if(button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
+	if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
 	{
-		iPlaySound("Background.wav", true);
+		iPlaySound("background.wav", true);
 	}
 }
 
 void iMouseWheel(int button, int dir, int mx, int my)
 {
-
 }
 /*
 	function iKeyboard() is called whenever the user hits a key in keyboard.
@@ -55,27 +54,27 @@ void iMouseWheel(int button, int dir, int mx, int my)
 void iKeyboard(unsigned char key)
 {
 
-	//place your codes for other keys here
+	// place your codes for other keys here
 }
 
 /*
 	function iSpecialKeyboard() is called whenver user hits special keys like-
-	function keys, home, end, pg up, pg down, arraows etc. you have to use 
+	function keys, home, end, pg up, pg down, arraows etc. you have to use
 	appropriate constants to detect them. A list is:
-	GLUT_KEY_F1, GLUT_KEY_F2, GLUT_KEY_F3, GLUT_KEY_F4, GLUT_KEY_F5, GLUT_KEY_F6, 
-	GLUT_KEY_F7, GLUT_KEY_F8, GLUT_KEY_F9, GLUT_KEY_F10, GLUT_KEY_F11, GLUT_KEY_F12, 
-	GLUT_KEY_LEFT, GLUT_KEY_UP, GLUT_KEY_RIGHT, GLUT_KEY_DOWN, GLUT_KEY_PAGE UP, 
-	GLUT_KEY_PAGE DOWN, GLUT_KEY_HOME, GLUT_KEY_END, GLUT_KEY_INSERT 
+	GLUT_KEY_F1, GLUT_KEY_F2, GLUT_KEY_F3, GLUT_KEY_F4, GLUT_KEY_F5, GLUT_KEY_F6,
+	GLUT_KEY_F7, GLUT_KEY_F8, GLUT_KEY_F9, GLUT_KEY_F10, GLUT_KEY_F11, GLUT_KEY_F12,
+	GLUT_KEY_LEFT, GLUT_KEY_UP, GLUT_KEY_RIGHT, GLUT_KEY_DOWN, GLUT_KEY_PAGE UP,
+	GLUT_KEY_PAGE DOWN, GLUT_KEY_HOME, GLUT_KEY_END, GLUT_KEY_INSERT
 */
 void iSpecialKeyboard(unsigned char key)
 {
 
-	if(key == GLUT_KEY_END)
+	if (key == GLUT_KEY_END)
 	{
-		exit(0);	
+		exit(0);
 	}
 
-	//place your codes for other keys here
+	// place your codes for other keys here
 }
 
 int main(int argc, char *argv[])
@@ -83,4 +82,4 @@ int main(int argc, char *argv[])
 	glutInit(&argc, argv);
 	iInitialize(600, 400, "Mousedemo");
 	return 0;
-}	
+}
