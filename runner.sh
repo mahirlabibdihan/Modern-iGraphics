7e4c7c536f33bca347825348f326564f35bb3097
mahirlabibdihan <mahirlabibdihan@gmail.com>
2025-05-16 20:22:17 +0600

    sdl2 in linux

--- runner.sh
+++ runner.sh
@@ -3,6 +3,8 @@
 # Exit immediately on error
 set -e
 
+# sudo apt install libsdl2-dev libsdl2-mixer-dev
+
 # Use default source file if none is provided
 SOURCE_FILE="${1:-iMain.cpp}"
 echo "Using source file: $SOURCE_FILE"
@@ -11,10 +13,10 @@ echo "Using source file: $SOURCE_FILE"
 mkdir -p bin obj
 
 # Compile the source file to an object file
-g++ -w -fexceptions -g -I. -IOpenGL/include -c "$SOURCE_FILE" -o obj/object.o
+g++ -w -fexceptions -g -I. -IOpenGL/include -IOpenGL/include/SDL2 -c "$SOURCE_FILE" -o obj/object.o
 
 # Link the object file to create the executable
-g++ -o bin/opengl obj/object.o -L./OpenGL/lib -lGL -lGLU -lglut -pthread
+g++ -o bin/opengl obj/object.o -lGL -lGLU -lglut -pthread -lSDL2 -lSDL2main -lSDL2_mixer
 
 # g++ -w -fexceptions -g -I. -IOpenGL/include -IOpenGL/include/SDL2 iMain.cpp -o bin/opengl.exe -static-libgcc -static-libstdc++ -lmingw32 -lSDL2main -lSDL2 -lSDL2_mixer -lOpenGL32 -lfreeglut && ./bin/opengl.exe
 
